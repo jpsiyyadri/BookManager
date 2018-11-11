60f1399fd65330e30e611980ccf692566ab890c5
Jaiprakash Siyyadri <jpsiyyadri@gmail.com>
2018-11-11 11:38:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ sudo apt-get install python-pip
 sudo pip install virtualenv
 sudo virtualenv venv
 source venv/bin/activate 
-sudo pip install flask 
+sudo pip install flask
+sudo pip install PyMySQL
 
